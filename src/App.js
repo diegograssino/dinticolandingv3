054356906e22b560825ce1ef059a3636a997548f
Diego Grassino <dg27ar@MacBook-Pro-de-Diego.local>
2021-12-23 12:08:46 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './components/MyNavbar';
+import './styles/custom.scss';
+import MyHeader from './components/MyHeader';
+
+function App() {
+  return (
+    <div>
+      <MyNavbar />
+      <MyHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Navbar, Nav, Offcanvas, Button } from 'react-bootstrap';
+
+const MyNavbar = () => {
+  return (
+    <Navbar bg="primary" variant="dark" expand={false}>
+      <Container fluid>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" className="me-5" />
+        <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="start">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">dintico</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+        <Navbar.Brand href="#">
+          <img className="logo-size" src={`${process.env.PUBLIC_URL}/assets/img/logodintico.png`} alt="Third slide" />
+        </Navbar.Brand>
+        <Button variant="outline-light" className="fs-6">
+          <small>INGRESAR</small>
+        </Button>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
