5fc75dce81c934487cf020c0a08f5dc6000d8263
Diego Grassino <dg27ar@MacBook-Pro-de-Diego.local>
2021-12-28 18:37:05 -0300

    Remaqueto botons redondeados

--- src/components/2Header/RoundedButtons.js
+++ src/components/2Header/RoundedButtons.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Row, Col, Container } from 'react-bootstrap';
 import { FaBell } from 'react-icons/fa';
 import { GoGear } from 'react-icons/go';
 import { BsGraphUp } from 'react-icons/bs';
 
 function RoundedButtons() {
   return (
-    <div className="position-relative min-height-20">
-      <div className="position-absolute top-50 start-50 translate-middle">
-        <Button
-          variant="primary"
-          className="fs-6 text-light rounded-circle mx-3 rounded-buttons-size"
-        >
-          <FaBell className="rounded-buttons-font-size" />
-        </Button>
-        <Button
-          variant="primary"
-          className="fs-6 text-light rounded-circle mx-3 rounded-buttons-size"
-        >
-          <GoGear className="rounded-buttons-font-size" />
-        </Button>
-        <Button
-          variant="primary"
-          className="fs-6 text-light rounded-circle mx-3 rounded-buttons-size"
-        >
-          <BsGraphUp className="rounded-buttons-font-size" />
-        </Button>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={4}>
+          <div className="position-relative min-height-20">
+            <div className="position-absolute top-50 start-100 translate-middle">
+              <Button
+                variant="primary"
+                className="fs-6 text-light rounded-circle mx-3 rounded-buttons-size rounded-buttons-shadow"
+              >
+                <FaBell className="rounded-buttons-font-size" />
+              </Button>
+            </div>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div className="position-relative min-height-20">
+            <div className="position-absolute top-50 start-50 translate-middle">
+              <Button
+                variant="primary"
+                className="fs-6 text-light rounded-circle mx-3 rounded-buttons-size rounded-buttons-shadow"
+              >
+                <GoGear className="rounded-buttons-font-size" />
+              </Button>
+            </div>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div className="position-relative min-height-20">
+            <div className="position-absolute top-50 start-0 translate-middle">
+              <Button
+                variant="primary"
+                className="fs-6 text-light rounded-circle mx-3 rounded-buttons-size rounded-buttons-shadow"
+              >
+                <BsGraphUp className="rounded-buttons-font-size" />
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
